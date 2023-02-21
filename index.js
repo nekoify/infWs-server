a6bf102c0a69e2b21334efd8eeca9bdd5606df51
nekoify <xl83dev@gmail.com>
2023-02-21 13:19:46 +1000

    h

--- index.js
+++ index.js
@@ -2,15 +2,9 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-let { DB } = require("mongquick");
-// const mongoSec = process.env.mongoSec
-// const mdb = new DB(`mongodb+srv://xl83:${mongoSec}@cluster0.c2sln.mongodb.net/Cluster0?retryWrites=true&w=majority`);
 
- // mdb.set(`info-${userLower}`, {pass: hash, user: data.username});
-// await mdb.get(`info-${checkLower}`)
-
-
-
+const accountData = require(`${__dirname}/account.json`)
+const chunkData = require(`${__dirname}/chunks.json`)
 
 
 function createElementFromHTML(htmlString) {
@@ -583,11 +577,13 @@ app.get('/', (req, res) => {
     res.send('server');
 });
 
-io.on('connection', async(socket) => {
-  
+io.on('connection', (socket) => {
+  socket.on("account", (data) => {
+    console.log(data)
+  })
  
 })
 
 server.listen(process.env.PORT || 8085, () => {
-    console.log('listening on *:3000');
+    console.log('listening on *:8085');
 })
