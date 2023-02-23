4d976820bbad618171aeb8e587b249dc5a14a1bd
nekoify <xl83dev@gmail.com>
2023-02-23 14:28:34 +1000

    nn

--- index.js
+++ index.js
@@ -920,6 +920,7 @@ client.on("messageCreate", (message) => {
     var string = message.content
     string = string.split(" ")
     if (string[0] == "!score") {
+        if (message.author.id == "640147303939964930" || message.author.id == "416508744097071107") {
         var names = {},
             ids = Object.keys(accountData)
         for (let i = 0; i < ids.length; i++) {
@@ -935,7 +936,7 @@ client.on("messageCreate", (message) => {
             message.channel.send(`Set score of ${string[1]} to ${string[2]}`)
         }
         
-    
+        }
     }
 })
 
@@ -953,7 +954,6 @@ client.on("messageCreate", (message) => {
 })
 
 client.on("messageCreate", async (message) => {
-    console.log(message.content)
     message.content = message.content.split(" ")
     if (message.content[0] == "!board") {
         
@@ -1000,7 +1000,6 @@ client.on("messageCreate", (message) => {
 })
 
 client.on("messageCreate", (message) => {
-    console.log(message.author.id)
     if (message.content == "!test") {
     if (message.author.id == "640147303939964930" || message.author.id == "416508744097071107") {
     message.channel.send("test")
