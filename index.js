c649c83e3cb87b75d90bfc11fe2ed3c9a6159550
aeolus-1 <benreef2018@outlook.com>
2024-05-22 09:36:30 +1000

    maybe a mine wall fix but idk

--- index.js
+++ index.js
@@ -593,7 +593,7 @@ function cTile(e, t) {
     this.pos = v(e, t)
     let dst = getDst(v(0,0),v(e,t))*0.2,
         num = 7-Math.pow(Math.sqrt(dst*0.13),3.2)
-    this.mine = Math.random()<(1/num)
+    this.mine = (1/num>=1)?Math.random()<(1/num):true
     this.uncovered = false
     this.flagged = false
     this.lootBox = (Math.random()<(0.5/100))?1:false
