978172121c8ec3d8ee0d782b5ba25d7546d635b3
aeolus-1 <benreef2018@outlook.com>
2024-05-22 13:43:13 +1000

    added one thing

--- index.js
+++ index.js
@@ -319,7 +319,7 @@ function acknowledgeAccount(id, name) {
         },
         selectedFlag:1,
     });
-    accountData[id].owns = accountData[id].owns||{}
+    accountData[id].owns = accountData[id].owns||{1:true}
     accountData[id].selectedFlag = accountData[id].selectedFlag||1
     accountData[id].name = name
     fs.writeFileSync(`${__dirname}/account.json`, JSON.stringify(accountData));
